35c8bff5d66ba7c3d3c0b3873d4ca3207c283487
phuongvy199 <caophuongvy199@gmail.com>
2020-07-22 08:35:17 -0500

    XD LOP DUNG CHUNG

---
+++ DUNGCHUNG/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DUNGCHUNG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLDB.SQLDB.ConnectionString = @"Data Source=.\SQLEXPRESS;Integrated Security=True";
+            SqlCommand sqlcmd = new SqlCommand("select * from KHACHHANG");
+            DataTable dt = SQLDB.SQLDB.GetData(sqlcmd);
+            Console.WriteLine(dt.Rows.Count.ToString());
+            Console.ReadKey();
+        }
+    }
+}
